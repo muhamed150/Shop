0632ba392996955fdc33a686df7489cd5bfb1f69
Мухамед Сали <muhamedsali01@pmg-haskovo.org>
2020-04-14 00:22:10 +0300

    Initialize NutController.cs

--- Shop/Controllers/NutController.cs
+++ Shop/Controllers/NutController.cs
@@ -1,10 +1,65 @@
-﻿using System;
+﻿using Shop.Data;
+using Shop.Data.Models;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace Shop.Controllers
 {
-    class NutController
+   public class NutController
     {
+        private ShopContext context;
+
+        public List<Nut> GetAllNuts()
+        {
+            using (context = new ShopContext())
+            {
+                return context.Nuts.ToList();
+            }
+        }
+
+        public Nut GetNutByName(string name) //ако се счупи промени на id
+        {
+            using (context = new ShopContext())
+            {
+                return context.Nuts.Find(name);
+            }
+        }
+
+        public void Add(Nut nut)
+        {
+            using (context = new ShopContext())
+            {
+                context.Add(nut);
+                context.SaveChanges();
+            }
+        }
+
+        public void Update(Nut nut)
+        {
+            using (context = new ShopContext())
+            {
+                var item = context.Drinks.Find(nut.Id);
+                if (item != null)
+                {
+                    context.Entry(item).CurrentValues.SetValues(nut);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public void Delete(Nut nut)
+        {
+            using (context = new ShopContext())
+            {
+                var item = context.Drinks.Find(nut.Id);
+                if (item != null)
+                {
+                    context.Drinks.Remove(item);
+                    context.SaveChanges();
+                }
+
+            }
+        }
+
     }
 }
