f17eca6bfb689c576938d636021a4c0be5c478b4
Мухамед Сали <muhamedsali01@pmg-haskovo.org>
2020-04-28 15:57:48 +0300

    Added countdown in splash screen

--- Shop/Program.cs
+++ Shop/Program.cs
@@ -14,38 +14,34 @@ namespace Shop
             //aTimer.Interval = 10;
             aTimer.AutoReset = false;
             aTimer.Start();
-            Thread.Sleep(4000);
-            Console.Clear();
+            Thread.Sleep(7000);
             Console.CursorVisible = true;
             Display display = new Display();
 
         }
         private static void OnTimedEvent(object source, ElapsedEventArgs e)
         {
+            for (int a = 6; a > 0; a--)
+            {
             Console.CursorVisible = false;
-            Console.WriteLine(new string(' ', 20));
-            Console.WriteLine(new string(' ', 20));
-            Console.WriteLine(new string(' ', 20));
-            Console.WriteLine(new string(' ', 20));
-            Console.WriteLine(new string(' ', 10));
+            //Console.WriteLine(new string(' ', 20));
+            //Console.WriteLine(new string(' ', 20));
+            //Console.WriteLine(new string(' ', 20));
+            //Console.WriteLine(new string(' ', 20));
+            //Console.WriteLine(new string(' ', 10));
             Console.Write(new string(' ', 2));
-            Console.WriteLine(new string('*', 41));
+            Console.WriteLine(new string('*', 42));
             Console.Write(new string(' ', 2));
-            Console.WriteLine("* SHOP Created by: Muhamed and Plmanena *");
+            Console.WriteLine("* Market Created by Muhamed and Plmanena *");
             Console.Write(new string(' ', 2));
-            Console.WriteLine(new string('*', 41));
+            Console.WriteLine(new string('*', 42));
+            Console.WriteLine("");
+            Console.Write("The program will start after: {0}", a);
+            System.Threading.Thread.Sleep(1000);
+            Console.Clear();
+            }
+
 
         }
-
-
-
-
-        //static void Main(string[] args)
-        //{
-        //        Display display = new Display();
-
-        //        // Restore original colors  
-        //        Console.ResetColor();
-        //}
     }
 }
