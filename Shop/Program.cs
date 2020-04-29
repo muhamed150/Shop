e63e61355463d5f3151bdf4c141224ffa31fd4ac
Мухамед Сали <muhamedsali01@pmg-haskovo.org>
2020-04-29 14:46:02 +0300

    added xml for Program.cs

--- Shop/Program.cs
+++ Shop/Program.cs
@@ -11,7 +11,6 @@ namespace Shop
         {
             System.Timers.Timer aTimer = new System.Timers.Timer();
             aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
-            //aTimer.Interval = 10;
             aTimer.AutoReset = false;
             aTimer.Start();
             Thread.Sleep(7000);
@@ -19,22 +18,22 @@ namespace Shop
             Display display = new Display();
 
         }
+        /// <summary>
+        /// Splash screen with countdown timer.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="e"></param>
         private static void OnTimedEvent(object source, ElapsedEventArgs e)
         {
             for (int a = 6; a > 0; a--)
             {
             Console.CursorVisible = false;
-            //Console.WriteLine(new string(' ', 20));
-            //Console.WriteLine(new string(' ', 20));
-            //Console.WriteLine(new string(' ', 20));
-            //Console.WriteLine(new string(' ', 20));
-            //Console.WriteLine(new string(' ', 10));
             Console.Write(new string(' ', 2));
-            Console.WriteLine(new string('*', 42));
+            Console.WriteLine(new string('*', 40));
             Console.Write(new string(' ', 2));
-            Console.WriteLine("* Market Created by Muhamed and Plmanena *");
+            Console.WriteLine("* Shop Created by Muhamed and Plmanena *");
             Console.Write(new string(' ', 2));
-            Console.WriteLine(new string('*', 42));
+            Console.WriteLine(new string('*', 40));
             Console.WriteLine("");
             Console.Write("The program will start after: {0}", a);
             System.Threading.Thread.Sleep(1000);
